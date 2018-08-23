f47fe55c8c74253cd60615cd665b80f17b1062e6
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-08-23 14:59:19 +0100

    Change default name of the output file to what Atlas uses, as-is

--- sh/atlas_species.sh
+++ sh/atlas_species.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script creates the species file for Expression Atlas based on bioentity annotations
 
-target=${1:-`pwd`/speciesProperties.json}
+target=${1:-`pwd`/species-properties.json}
 
 amm -s `dirname $0`/../src/atlas/AtlasSpecies.sc > $target
 
